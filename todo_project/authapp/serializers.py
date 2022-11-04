5c361c88b664e3ce2d8d9e993ac6fb9e43167b94
Willy Wonka <Woodenehad9798740@yandex.ru>
2022-11-04 18:38:58 +0300

    todo_notes: 1 Introduction

---
+++ todo_project/authapp/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework.serializers import ModelSerializer
+from authapp.models import User
+
+
+class UserModelSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        ]
---
+++ todo_project/authapp/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from authapp.models import User
+from authapp.serializers import UserModelSerializer
+
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
